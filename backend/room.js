64ccee191cc974824ab1939f0b37171eb9faace4
RoyCohenGit <rtcohen99@gmail.com>
2021-05-07 21:39:10 +0200

    Add function to initialize a peer for an already existing user and establish a connection.

--- backend/room.js
+++ backend/room.js
@@ -12,23 +12,41 @@ const room = (params) => {
     const peers = useRef();
     const roomID = params.roomID;
 
-    useEffect(() =>{
+    useEffect(() => {
         socket.current = io.connect("/");
         navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(stream => {
             userVoice.current.srcObject = stream;
             socket.current.emit("join", roomID);
             socket.current.on("user list", users => {
                 //Iterate through each user present in the room and create a local peer for each of the users in the room.
-                for (id of users){
-                    const peer = initPeer(id, socket.current.id, stream);
+                for (ID of users) {
+                    const peer = initPeer(ID, socket.current.id, stream);
                     peers.current.push({
-                        peerID: id,
+                        peerID: ID,
                         peer
                     })
                 }
             })
         })
     })
+
+    //Initialise a new peer
+    function initPeer(existingUserID, newUserID, stream) {
+
+        //Create peer object
+        const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream,
+        })
+
+        //sent a signal to the existing user to start establishing a connection.
+        peer.on("signal", signal => {
+            socket.current.emit("sending signal", {existingUserID, newUserID, signal})
+        })
+
+        return peer;
+    }
 }
 //Adds a peers to the room
 function addPeer (signal, newPeerID, stream) {
