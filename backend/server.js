8e5c79d71bfa20023ed109606ead0534f69257ad
Jaron Cohen <CHNJAR003@myuct.ac.za>
2021-05-08 10:17:24 +0200

    Implement enter room event listener in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,37 @@ const socket = require("socket.io");
 const io = socket(server);
 
 const users = {}; //Users object stores room objects with objects to store socket information of users in each room
+const userSocketRoomMap = {}; //Store object of user socket ids with room ids for disconnection
 
 io.on("connection", (socket) => {
   console.log("new connection");
 
   //User attempts to enter a room
-  socket.on("enter room", (roomID, name) => {});
+  socket.on("enter room", (roomID, name) => {
+    console.log("enter room: " + roomID);
+
+    if (users[roomID]) {
+      const length = users[roomID].length; //Get how many users are already in room
+
+      //Limit to 4 users for performance reasons for now - (mesh network = complete graph on n vertices = many connections)
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push({ id: socket.id, name: name }); //Add id of socket and name user entered into frontend
+    } else {
+      users[roomID] = [{ id: socket.id, name: name }]; //Initialise the room array
+    }
+
+    userSocketRoomMap[socket.id] = roomID;
+
+    const otherUsersInThisRoom = users[roomID].filter(
+      ({ id, name }) => id !== socket.id
+    ); //users other than one asking to join
+
+    socket.emit("other users", otherUsersInThisRoom);
+    console.log("users in (" + roomID + "): ", otherUsersInThisRoom);
+  });
 
   //Signal sent to other users that new user has joined
   socket.on("forward signal", (payload) => {});
