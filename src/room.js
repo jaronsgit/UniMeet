912c361f82882eb6574af0d72fdacd69b100aea0
Jaron Cohen <CHNJAR003@myuct.ac.za>
2021-05-08 10:47:24 +0200

    Create room.js Add rough outline structure to room.js for enter room flow

---
+++ src/room.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import Peer from "simple-peer";
+
+const Room = (props) => {
+  const [peers, setPeers] = useState([]);
+  const socketRef = useRef(); //Store ref to underlying socket io socket for server
+  const userVideo = useRef(); //Store ref to user video/audio stream
+  const peersRef = useRef([]);
+  const { roomID } = useParams(); //Get room id from url path
+  const { name } = props;
+
+  //Use effect to run once when we enter room for first time
+  useEffect(() => {
+    socketRef.current = io.connect("/"); //connect to server endpoint and get socket to server
+    //Get access to device media stream - need to ask for permission - remember https for later
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        userVideo.current.srcObject = stream;
+        socketRef.current.emit("enter room", roomID, name);
+        socketRef.current.on("other users", (users) => {
+          console.log("users received: ", users);
+        });
+
+        socketRef.current.on("user entered", (payload) => {
+          console.log("user entered: ", payload);
+        });
+
+        socketRef.current.on("returned signal", (payload) => {
+          console.log("returned signal: ", payload);
+        });
+      });
+  }, []);
+
+  //Create peer for other users received already in room
+  function createOtherUserPeer(userToSignal, socketID, stream, name) {
+    console.log("createOtherUserPeer: ", userToSignal, socketID, stream, name);
+  }
+
+  //Add new peer that entered the room we are already in
+  function addNewPeer(incomingSignal, socketID, stream, name) {
+    console.log("addNewPeer: ", incomingSignal, socketID, stream, name);
+  }
+
+  return <div></div>;
+};
+
+export default Room;
